63db40c74bc996b797a3f1dbfdcae275197ed833
Simon <simon@Simons-MacBook-Air.local>
2018-11-12 17:47:15 +0100

    a

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,13 +1,23 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
+# no boundary is assumed.
 def PerformRandomWalk(agentMatrix):
+
 	end
 
 def PerformInfection(agentMatrix):
+
+	end
+def Initialize agentMatrix(timeSteps, latticeSize):
+	agentMatrix = np.zeros((timeSteps,3,latticeSize,latticeSize)) # first parameter: timestep, second: nbrOfStates, rest is standard m x n matrix type.
+	for iRow in range(1,latticeSize):
+		for jCol in range(1,latticeSize):
+			for kState in range(1,3):
+
+
+	return agentMatrix;
 	end
 
 d = 0.8; beta = 0.6; gamma = 0.01; 
 timeSteps = 3; latticeSize = 100; numberOfAgents = 1000;
-agentMatrix = np.zeros((timeSteps,3,latticeSize,latticeSize)) # first parameter: timestep, second: nbrOfStates, rest is standard m x n matrix type.
-print(agentMatrix)
