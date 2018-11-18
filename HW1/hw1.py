eb4f93d865c0229c57cd8a178026ec10cbfa3e1a
Simon <simon@Simons-MacBook-Air.local>
2018-11-18 10:49:36 +0100

    a

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,55 +1,170 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+from matplotlib import colors
+from matplotlib.animation import FuncAnimation
+
+
 
 # no boundary is assumed.
 def PerformRandomWalk(agentMatrix, d):
-	updatedAgentMatrix = agentMatrix
+	updatedAgentMatrix = agentMatrix.copy()
 	for iRow in range(0,latticeSize):
 		for jCol in range(0,latticeSize):
-			for kState in range(0,2):
-				nbrOfAgentsAtCurrentStateSite = agentMatrix[kState,iRow,jCol]
-				for iAgentOnCurrentStateSite in range(0,nbrOfAgentsAtSite):
+			for kState in range(0,3):
+				nbrOfAgentsAtCurrentStateSite = agentMatrix[iRow,jCol,kState]
+				for iAgentOnCurrentStateSite in range(0,int(nbrOfAgentsAtCurrentStateSite)):
 					if(np.random.rand(1) < d):
-						updatedAgentMatrix[kState,iRow,jCol] -= 1
-						if(0 < np.random.rand(1) < 0.25):
-							updatedAgentMatrix[kState, (1 + iRow) % latticeSize,jCol] += 1
-						else if(0.25 < np.random.rand(1) < 0.5):
-							updatedAgentMatrix[kState, (iRow - 1) % latticeSize,jCol] += 1
-						else if(0.5 < np.random.rand(1) < 0.75):
-							updatedAgentMatrix[kState, iRow, (jCol + 1) % latticeSize] += 1
-						else if(0.75 < np.random.rand(1) < 1):
-							updatedAgentMatrix[kState, iRow, (jCol - 1) % latticeSize] += 1	
+						updatedAgentMatrix[iRow,jCol,kState] -= 1
+						r = np.random.rand(1)
+						if(0 < r < 0.25):
+							updatedAgentMatrix[(1 + iRow) % latticeSize,jCol,kState] += 1
+						elif(0.25 < r < 0.5):
+							updatedAgentMatrix[(iRow - 1) % latticeSize,jCol,kState] += 1
+						elif(0.5 < r < 0.75):
+							updatedAgentMatrix[iRow, (jCol + 1) % latticeSize,kState] += 1
+						elif(0.75 < r < 1):
+							updatedAgentMatrix[iRow, (jCol - 1) % latticeSize,kState] += 1	
 	return updatedAgentMatrix
 
-
 def PerformInfectionAndRecovery(agentMatrix, beta, gamma):
-	updatedAgentMatrix = agentMatrix
+	updatedAgentMatrix = agentMatrix.copy()
 	for iRow in range(0,latticeSize):
 		for jCol in range(0,latticeSize):
-			nbrOfInfectedAtCurrentSite = agentMatrix[1,iRow,jCol]
-			for iInfectedAtCurrentSite in range(0,nbrOfInfectedAtCurrentSite):
+			nbrOfInfectedAtCurrentSite = agentMatrix[iRow,jCol,1]
+			for iInfectedAtCurrentSite in range(0,int(nbrOfInfectedAtCurrentSite)):
 				if(np.random.rand(1) < beta):
-					updatedAgentMatrix[0,iRow,jCol] = 0
-					updatedAgentMatrix[1,iRow,jCol] += agentMatrix[0,iRow,jCol]
-				else if(np.random.rand(1) < gamma):
-					updatedAgentMatrix[1,iRow,jCol] -= 1
-					updatedAgentMatrix[2,iRow,jCol] += 1
+					updatedAgentMatrix[iRow,jCol,0] = 0
+					updatedAgentMatrix[iRow,jCol,1] += agentMatrix[iRow,jCol,0]
+				if(np.random.rand(1) < gamma):
+					updatedAgentMatrix[iRow,jCol,1] -= 1
+					updatedAgentMatrix[iRow,jCol,2] += 1
 	return updatedAgentMatrix
 
 
 def InitializeAgentMatrix(numberOfAgents, latticeSize):
-	agentMatrix = np.zeros((3,latticeSize,latticeSize)) # 0: susc, 1: inf, 2: rec
+	agentMatrix = np.zeros((latticeSize,latticeSize,3)) # 0: susc, 1: inf, 2: rec
 	for iAgent in range(0,numberOfAgents):
 		x = np.random.randint(latticeSize)
 		y = np.random.randint(latticeSize)
 		infectionLength = np.sqrt((1 - ratio)) * latticeSize
 		if(latticeSize / 2 - infectionLength / 2 <= x <= latticeSize / 2 + infectionLength / 2 and
 			latticeSize / 2 - infectionLength / 2 <= y <= latticeSize / 2 + infectionLength / 2):
-			agentMatrix[1, y, x] += 1
+			agentMatrix[y, x, 1] += 1
 		else:
-			agentMatrix[0, y, x] += 1
+			agentMatrix[y, x, 0] += 1
 	return agentMatrix
 
+
+
+fig, ax = plt.subplots(figsize=(5, 8))
+def update(t):
+    ax.imshow(storedAgentMatrix[:,:,:,t])
+    ax.set_title('Random walk')
+
+
+runRandomWalk = False; runMainSimulation = True
+
 d = 0.8; beta = 0.6; gamma = 0.01
-latticeSize = 100; numberOfAgents = 1000
-ratio = 0.9
+latticeSize = 10; numberOfAgents = 1; ratio = 0.9
+timeStepsForRandomWalk = 40
+storedAgentMatrix = np.zeros((latticeSize, latticeSize, 3 , timeStepsForRandomWalk))
+t = 0
+if(runRandomWalk == True):
+	agentMatrix = InitializeAgentMatrix(numberOfAgents, latticeSize)
+	for t in range(0,timeStepsForRandomWalk):
+		agentMatrix = PerformRandomWalk(agentMatrix,d)
+		storedAgentMatrix[:,:,:,t] = agentMatrix.copy()
+	# create gif for random walk
+	anim = FuncAnimation(fig, update, frames=np.arange(0, timeStepsForRandomWalk), interval=50)
+	anim.save('randomWalk.gif', dpi=80, writer='imagemagick')
+	plt.close()
+
+
+
+d = 0.8;
+latticeSize = 100; numberOfAgents = 1000; ratio = 0.99
+gammaRuns = np.array([0.7,0.5,0.4,0.3, 0.2, 0.1, 0.08, 0.01])
+betaRuns = np.array([0.8, 0.4])
+if(runMainSimulation == True):
+	nRuns = 10
+	R_storedRuns = np.zeros(( nRuns, np.size(betaRuns) , np.size(gammaRuns) ))
+	for iRun in range(0,nRuns):
+		print iRun
+		iBeta = 0
+		for beta in betaRuns:
+			iGamma = 0
+			for gamma in gammaRuns:
+				agentMatrix = InitializeAgentMatrix(numberOfAgents, latticeSize)
+				SnbrSusceptible = np.array([])
+				SnbrInfected = np.array([])
+				SnbrRecovered = np.array([])
+				agentMatrixSaved = np.array([])
+				t = 0; nbrInfected = np.sum(agentMatrix[:,:,1]); showDynamics = False; tSaveMatrix = 100
+				saveMatrix = False; matrixSaved = False; plotPopulationDynamics = False; plotR = True
+				while(nbrInfected != 0 and nbrInfected < numberOfAgents):
+					agentMatrix = PerformInfectionAndRecovery(agentMatrix, beta, gamma)
+					agentMatrix = PerformRandomWalk(agentMatrix,d)
+
+					nbrSusceptible = np.sum(agentMatrix[:,:,0])
+					nbrInfected = np.sum(agentMatrix[:,:,1])
+					nbrRecovered = np.sum(agentMatrix[:,:,2])
+					SnbrSusceptible = np.append(SnbrSusceptible,nbrSusceptible)
+					SnbrInfected = np.append(SnbrInfected,nbrInfected)
+					SnbrRecovered = np.append(SnbrRecovered,nbrRecovered)
+					
+					if(showDynamics == True): 
+						im1 = plt.imshow(agentMatrix)	
+						plt.title('Time step: %i' %t)
+						plt.ion()
+						plt.show()
+						plt.pause(0.1)
+
+					#if((t % 100) == 0):
+					#	print t
+					if(t == tSaveMatrix and saveMatrix):
+						agentMatrixSaved = agentMatrix.copy()
+						matrixSaved = True
+
+					t += 1	
+				R_storedRuns[iRun,iBeta,iGamma] = nbrRecovered
+				iGamma += 1
+
+				if(plotPopulationDynamics == True):
+					St = np.arange(np.size(SnbrInfected))
+					plt.figure(1)
+					plt.plot(St, SnbrInfected,'r')
+					plt.plot(St, SnbrRecovered,'g')
+					plt.plot(St, SnbrSusceptible,'b')
+
+					plt.title('d = %(d)f, beta = %(beta)f and gamma = %(gamma)f' %{'d' :d, 'beta': beta, 'gamma' : gamma})
+					plt.legend(['Infected','Recovered','Susceptible'])
+					plt.xlabel('Time step')
+					plt.ylabel('Number of agents')
+					plt.show()
+
+				if(matrixSaved == True):
+					plt.figure(2)
+					im1 = plt.imshow(agentMatrixSaved)
+					plt.title('Time step: %i' %tSaveMatrix)
+
+			iBeta += 1
+R = np.average(R_storedRuns,0)
+
+plt.figure(1)
+
+plt.plot(betaRuns[0] / gammaRuns, R[0,:])
+plt.plot(betaRuns[1] / gammaRuns, R[1,:])
+
+plt.ylabel('R_inf')
+plt.xlabel('beta / gamma')
+plt.legend([betaRuns[0], betaRuns[1]])
+plt.show()
+
+
+
+
+	
+
+
+
+	
--- HW1/test.py
+++ HW1/test.py
@@ -1,12 +1,8 @@
-import numpy as np
+from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
-
-#a = [[0 0 1][0 2 2][1 2 2]]
-b = np.zeros((3,3))
-a = np.array([[1,2,2],[1,0,0],[1,1,1]])
-plt.matshow(a)
-plt.show()
+import numpy as np
 
 
+a = np.ones((2,4,5))
 
-
+print np.average(a,0)
